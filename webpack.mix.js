2f18ae08ce18f4a1901bf822461c06ccd1018535
Alex Mills <alex@asdfx.us>
2019-02-07 18:47:35 +0000

    updated options to output fonts to public/fonts

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,9 @@ let mix = require('laravel-mix');
 
 mix
     .js('resources/js/application.js', 'public/js')
-    .sass('resources/sass/application.scss', 'public/css');
+    .sass('resources/sass/application.scss', 'public/css')
+    .options({
+        fileLoaderDirs: {
+            fonts: 'public/fonts'
+        }
+    });
